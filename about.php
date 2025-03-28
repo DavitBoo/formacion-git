959a7412c100e1cd5bf7deebe7214745aac48cf9
jjuarez11 <j.juarez.caronte@gmail.com>
2025-03-28 11:42:49 +0100

    Hola que tal

--- about.php
+++ about.php
@@ -2,5 +2,6 @@
 <main>
     <h1>Sobre Nosotros</h1>
     <p>Somos una agencia especializada en marketing digital y desarrollo web con más de 10 años de experiencia.</p>
+    <p>Nuestro equipo está formado por expertos en control de versiones y desarrollo ágil.</p>
 </main>
 <?php include 'includes/footer.php'; ?>
