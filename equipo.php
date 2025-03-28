4413307a0c3174062a5a30fbb2c42e119a72d7f6
Alberto Bravo <a.bravo@carontestudio.com>
2025-03-28 11:45:09 +0100

    Añadir equipo

--- equipo.php
+++ equipo.php
@@ -2,5 +2,10 @@
 <main>
     <h1>Nuestro equipo</h1>
     <p>Juanfe</p>
+    <p>Jon</p>
+    <p>Víctor</p>
+    <p>David</p>
+    <p>Bea</p>
+    <p>Kristina</p>
 </main>
 <?php include 'includes/footer.php'; ?>
