72920885ea8410ec718e375eac542ece5db2ea49
jjuarez11 <j.juarez.caronte@gmail.com>
2025-03-28 11:58:21 +0100

    Pacman de un proyecto de github

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PracticaGit</title>
     <link rel="stylesheet" href="css/styles.css">
+    <script type="text/javascript" src="lib/config/config.js"></script>
+    <script type="text/javascript" src="lib/utils.js"></script>
+    <script type="text/javascript" src="lib/classes/assetsmanager.js"></script>
+    <script type="text/javascript" src="lib/classes/display.js"></script>
+    <script type="text/javascript" src="lib/classes/controller.js"></script>
+    <script type="text/javascript" src="lib/classes/animator.js"></script>
+    <script type="text/javascript" src="lib/classes/mobile.js"></script>
+    <script type="text/javascript" src="lib/classes/tileset.js"></script>
+    <script type="text/javascript" src="lib/classes/frame.js"></script>
+    <script type="text/javascript" src="lib/classes/ghost.js"></script>
+    <script type="text/javascript" src="lib/classes/inky.js"></script>
+    <script type="text/javascript" src="lib/classes/pinky.js"></script>
+    <script type="text/javascript" src="lib/classes/clyde.js"></script>
+    <script type="text/javascript" src="lib/classes/blinky.js"></script>
+    <script type="text/javascript" src="lib/classes/pacman.js"></script>
+    <script type="text/javascript" src="lib/classes/balls.js"></script>
+    <script type="text/javascript" src="lib/classes/world.js"></script>
+    <script type="text/javascript" src="lib/classes/game.js"></script>
+    <script type="text/javascript" src="lib/app.js"></script>
 </head>
 <body>
     <header>
--- index.php
+++ index.php
@@ -4,5 +4,19 @@
     <p>Expertos en marketing digital y desarrollo web</p>
     <p>Somo expertos en control de versiones</p>
     <p>puedo cambiar cosas?</p>
+  
+
+
+    <div id="main">
+      <header>
+        <div id="score">0</div>
+        <div id="volume"></div>
+      </header>
+      <canvas id="canvas"></canvas>
+      <div id="lives">0</div>
+      <!--<canvas id="path"></canvas>-->
+    </div>
+  </body>
+
 </main>
 <?php include 'includes/footer.php'; ?>
